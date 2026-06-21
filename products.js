0b1c6aef8a2adf4e7ce83acf8b242073bbca12d8
toolbykien <nguyenthanhgiangrlbz1266@gmail.com>
2026-06-21 23:22:30 +0700

    admin: cập nhật danh sách thực đơn sản phẩm & danh mục

--- products.js
+++ products.js
@@ -286,5 +286,12 @@ export const PRODUCTS = [
     "name": "Bánh kem petit oreo dâu tây",
     "desc": "Bánh kem cốt socola, mứt dâu tây kết hợp cùng kem tươi vị oreo, trang trí phủ bên trên lớp socola sệt và dâu tây cùng những chiếc \"petit oreo\" nhỏ xinh",
     "img": "https://r2.cms.4handy.vn/banh-kem-petit-oreo-dau-tay-1024x1024.jpg"
+  },
+  {
+    "id": "C020",
+    "cat": "cake1",
+    "name": "Bánh kem whipping ô long nhài nhãn mix su kem",
+    "desc": "Bánh kem cốt ô long và lớp kem whipping ô long nhài, kết hợp thạch trà bá tước và xốt trà ô long trong nhân bánh càng thêm phần đậm vị, bên ngoài là những chiếc bánh su kem Singapore với lớp vỏ dai mềm, nhân kem ngậy béo, các loại hoa quả tươi: xoài, nhãn, việt quất. Một chiếc biếc bánh ngọt ngào dành tặng Mẹ",
+    "img": "https://r2.cms.4handy.vn/banh-kem-whipping-o-long-nhai-nhan-mix-su-kem-main-1024x1024.jpg"
   }
 ];
